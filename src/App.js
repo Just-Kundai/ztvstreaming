071d4f4faa15527b6f980259433a711fb43a3a5b
Just-Kundai <kundaikhuleya@outlook.com>
2023-06-26 22:18:46 +1000

    adding API

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import MovieList from './MovieList';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch('https://moviesdatabase.p.rapidapi.com/titles/seasons/%7BseriesId%7D');
+        const data = await response.json();
+        setMovies(data);
+      } catch (error) {
+        console.log('Error fetching movies:', error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>TV Streaming Website</h1>
+      <MovieList movies={movies} />
+    </div>
+  );
+};
+
+export default App;
